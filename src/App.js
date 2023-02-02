1b89b7720eb8e42b9d347a5802b5503d4eb124df
Enes Kaya <eneskaya5261@gmail.com>
2023-02-02 13:48:17 +0300

    feat(pages): create home page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Topbar from "./components/topbar/Topbar";
+import Home from "./pages/home/Home";
 
 function App() {
   return (
     <>
       <Topbar />
+      <Home />
     </>
   );
 }
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,11 @@
+import Header from "../../components/header/Header"
+import "./home.css"
+
+export default function Home() {
+  return (
+    <div className="home">
+      <Header />
+      homepage
+    </div>
+  )
+}
