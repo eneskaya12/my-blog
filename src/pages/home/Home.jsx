28882d69590f24748d1e3b4fdbcd15484b154ec3
Enes Kaya <eneskaya5261@gmail.com>
2023-02-02 16:45:44 +0300

    feat(components): create posts component

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,16 @@
 import Header from "../../components/header/Header"
+import Posts from "../../components/posts/Posts"
+import Sidebar from "../../components/sidebar/Sidebar"
 import "./home.css"
 
 export default function Home() {
   return (
-    <div className="home">
+    <>
       <Header />
-      homepage
-    </div>
+      <div className="home">
+        <Posts />
+        <Sidebar />
+      </div>
+    </>
   )
 }
