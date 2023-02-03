9732f513c7d12ee89d50cd91ccfcd423bed1ca13
Enes Kaya <eneskaya5261@gmail.com>
2023-02-03 13:08:35 +0300

    feat(pages): create single page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import Topbar from "./components/topbar/Topbar";
-import Home from "./pages/home/Home";
+import Single from "./pages/single/Single";
 
 function App() {
   return (
     <>
       <Topbar />
-      <Home />
+      <Single />
     </>
   );
 }
