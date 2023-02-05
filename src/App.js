7120e1da2dbd6befa64880cc09ff6e8f8d399bda
Enes Kaya <eneskaya5261@gmail.com>
2023-02-05 12:43:40 +0300

    feat(App.js): add Routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import Topbar from "./components/topbar/Topbar";
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import Write from "./pages/write/Write";
+import Settings from "./pages/settings/Settings";
+import Single from "./pages/single/Single";
 import Register from "./pages/register/Register";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 
 function App() {
+  const user = false;
   return (
-    <>
-      <Topbar />
-      <Register />
-    </>
+    <Router>
+      {user ? <Topbar /> : true}
+      <Routes>
+        <Route path="/" element={user ? <Home /> : <Login />}/>
+        <Route path="/register" element={user ? <Navigate replace to="/" /> : <Register />}/>
+        <Route path="/login" element={user ? <Navigate replace to="/" /> : <Login />}/>
+        <Route path="/write" element={user ? <Write /> : <Navigate replace to="/"/>}/>
+        <Route path="/settings" element={user ? <Settings /> : <Navigate replace to="/"/>}/>
+        <Route path="/post/:postId" element={<Single />}/>
+      </Routes>
+    </Router>
   );
 }
 
