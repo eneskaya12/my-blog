154834760483ad5c1063bd486e8e20bfdb83117a
Enes Kaya <eneskaya5261@gmail.com>
2023-02-19 18:41:08 +0300

    feat(context): create AuthContext.js & AuthActions.js & AuthReducer.js

---
+++ src/context/auth/AuthActions.js
@@ -0,0 +1,12 @@
+export const LoginStart = (userCredentials) => ({
+  type: "LOGIN_START",
+});
+
+export const LoginSuccess = (user) => ({
+  type: "LOGIN_SUCCESS",
+  payload: user,
+});
+
+export const LoginFailure = () => ({
+  type: "LOGIN_FAILURE",
+});
