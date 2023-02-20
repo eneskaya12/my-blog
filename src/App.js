c4df5bba48b8a3cbaf386323d8c00ec6c8b7ccc9
Enes Kaya <eneskaya5261@gmail.com>
2023-02-20 12:21:09 +0300

    feat(src): update login system with context API

--- src/App.js
+++ src/App.js
@@ -6,9 +6,11 @@ import Settings from "./pages/settings/Settings";
 import Single from "./pages/single/Single";
 import Register from "./pages/register/Register";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from "./context/auth/AuthContext";
 
 function App() {
-  const user = true;
+  const {user} = useContext(AuthContext);
   return (
     <Router>
       {user ? <Topbar /> : true}
--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,16 +1,39 @@
+import { useContext, useRef } from "react";
 import { Link } from "react-router-dom";
 import "./login.css";
+import { AuthContext } from "../../context/auth/AuthContext";
+import axios from "axios";
 
 export default function Login() {
+
+  const userRef = useRef();
+  const passwordRef = useRef();
+  const { dispatch, isFetching } = useContext(AuthContext);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    dispatch({type:"LOGIN_START"});
+    try {
+      const res = await axios.post("/auth/login", {
+        username: userRef.current.value,
+        password: passwordRef.current.value
+      });
+      dispatch({type:"LOGIN_SUCCESS", payload:res.data});
+    } catch (err) {
+      dispatch({type:"LOGIN_FAILURE"});
+      console.log(err);
+    }
+  }
+  
   return (
     <div className="login">
       <span className="loginTitle">Login</span>
-      <form className="loginForm">
+      <form className="loginForm" onSubmit={handleSubmit}>
         <label>Username</label>
-        <input type="text" placeholder="Enter your username..." />
+        <input type="text" placeholder="Enter your username..." ref={userRef}/>
         <label>Password</label>
-        <input type="password" placeholder="Enter your password..." />
-        <button className="loginButton">Login</button>
+        <input type="password" placeholder="Enter your password..." ref={passwordRef}/>
+        <button className="loginButton" type="submit" disabled={isFetching}>Login</button>
       </form>
       <button className="loginRegisterButton">
         <Link className="link" to="/register">Register</Link>
