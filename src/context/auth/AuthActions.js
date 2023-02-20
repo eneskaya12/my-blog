4e303d8f08ec37225743826e22d8b582c75ae138
Enes Kaya <eneskaya5261@gmail.com>
2023-02-20 15:10:50 +0300

    feat(context): add logout feature

--- src/context/auth/AuthActions.js
+++ src/context/auth/AuthActions.js
@@ -10,3 +10,7 @@ export const LoginSuccess = (user) => ({
 export const LoginFailure = () => ({
   type: "LOGIN_FAILURE",
 });
+
+export const Logout = () => ({
+  type: "LOGOUT",
+});
--- src/context/auth/AuthReducer.js
+++ src/context/auth/AuthReducer.js
@@ -18,6 +18,12 @@ const AuthReducer = (state, action) => {
         isFetching: false,
         error: true,
       };
+    case "LOGOUT":
+      return {
+        user: null,
+        isFetching: false,
+        error: false,
+      };
 
     default:
       return state;
