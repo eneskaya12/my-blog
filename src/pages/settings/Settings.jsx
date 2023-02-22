85a72fcd0c12b8c48cb5ee837a57063af12015f1
Enes Kaya <eneskaya5261@gmail.com>
2023-02-22 09:38:33 +0300

    feat(settings): add update user feature with context API

--- src/pages/settings/Settings.jsx
+++ src/pages/settings/Settings.jsx
@@ -1,7 +1,54 @@
 import "./settings.css"
 import Sidebar from "../../components/sidebar/Sidebar"
+import { useContext, useState } from "react"
+import { AuthContext } from "../../context/auth/AuthContext"
+import axios from "axios";
 
 export default function Settings() {
+
+  const [file, setFile] = useState(null);
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [success, setSuccess] = useState(false);
+
+  const { user, dispatch } = useContext(AuthContext);
+  const PF = "http://localhost:8800/images/";
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    dispatch({type:"UPDATE_START"});
+    const updatedUser = {
+      userId: user._id,
+      username,
+      email,
+      password
+    };
+
+    if(file){
+      const data = new FormData();
+      const filename = Date.now() + file.name;
+
+      data.append("name", filename);
+      data.append("file", file);
+      updatedUser.profilePicture = filename;
+
+      try {
+        await axios.post("/upload", data);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+    
+    try {
+      const res = await axios.put("/users/" + user._id, updatedUser);
+      setSuccess(true);
+      dispatch({type:"UPDATE_SUCCESS", payload:res.data});
+    } catch (err) {
+      dispatch({type:"UPDATE_FAILURE"});
+    }
+  };
+
   return (
     <div className="settings">
       <div className="settingsWrapper">
@@ -9,22 +56,25 @@ export default function Settings() {
           <span className="settingsUpdateTitle">Update Your Account</span>
           <span className="settingsDeleteTitle">Delete Account</span>
         </div>
-        <form className="settingsForm">
+        <form className="settingsForm" onSubmit={handleSubmit}>
           <label>Profile Picture</label>
           <div className="settingsPP">
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQCdUtb_N_bj04YdApVfv00WaakO8-c4t7HuA&usqp=CAU" alt=""/>
+            <img src={file ? URL.createObjectURL(file) : PF + user.profilePicture} alt=""/>
             <label htmlFor="fileInput">
               <i className="settingsPPIcon far fa-user-circle"></i>
             </label>
-            <input type="file" id="fileInput" style={{display:"none"}}/>
+            <input type="file" id="fileInput" style={{display:"none"}} onChange={(e) => setFile(e.target.files[0])}/>
           </div>
           <label>Username</label>
-          <input type="text" placeholder="Enes"/>
+          <input type="text" placeholder={user.username} onChange={(e) => setUsername(e.target.value)}/>
           <label>Email</label>
-          <input type="email" placeholder="enes@gmail.com"/>
+          <input type="email" placeholder={user.email} onChange={(e) => setEmail(e.target.value)}/>
           <label>Password</label>
-          <input type="password"/>
-          <button className="settingsSubmit">Update</button>          
+          <input type="password" onChange={(e) => setPassword(e.target.value)}/>
+          <button className="settingsSubmit" type="submit">Update</button>
+          {
+            success && <span style={{color:"green", textAlign:"center", marginTop:"20px"}}>Profile has been updated...</span>
+          }          
         </form>
       </div>
       <Sidebar />
