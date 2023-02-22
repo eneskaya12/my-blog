cdbd18cbd58bae774d10eded259320166d4f1d8f
Enes Kaya <eneskaya5261@gmail.com>
2023-02-22 09:35:51 +0300

    feat(context): add update feature

--- src/context/auth/AuthActions.js
+++ src/context/auth/AuthActions.js
@@ -14,3 +14,16 @@ export const LoginFailure = () => ({
 export const Logout = () => ({
   type: "LOGOUT",
 });
+
+export const UpdateStart = (userCredentials) => ({
+  type: "UPDATE_START",
+});
+
+export const UpdateSuccess = (user) => ({
+  type: "UPDATE_SUCCESS",
+  payload: user,
+});
+
+export const UpdateFailure = () => ({
+  type: "UPDATE_FAILURE",
+});
